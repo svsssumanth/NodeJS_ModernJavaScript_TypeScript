8c4b1418121845e2d61f38fecd554b2a078dba65
svsssumanth <s-v-s-s-sumanth.s-v-s-s-sumanth@capgemini.com>
2023-06-27 13:44:28 +0530

    Union Types (Adding | in variable decalartion so that it accepts different types of values)

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const num2Element = document.getElementById('num2');
 const buttonElement = document.querySelector('button');
 //const buttonElement = document.querySelector('button')!;  putting ! at the end tells the ts to ignore if this is null
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') { // type guard as we are running different code for different conditions
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
 buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
+    const stringResult = add(num1, num2);
     console.log(result);
+    console.log(stringResult);
 });
--- app.ts
+++ app.ts
@@ -3,14 +3,21 @@ const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button') as HTMLButtonElement;
 //const buttonElement = document.querySelector('button')!;  putting ! at the end tells the ts to ignore if this is null
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+function add(num1: number | string, num2: number | string) { // Union TYpes are declared using Pipe symbol
+  if (typeof num1 === 'number' && typeof num2 === 'number') {  // type guard as we are running different code for different conditions
+    return num1 + num2;
+  } else if (typeof num1 === 'string' && typeof num2 === 'string') {
+    return num1 + ' ' + num2;
+  }
+  return +num1 + +num2;
+} 
 
 
 buttonElement?.addEventListener('click', () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = add(+num1, +num2);
+  const stringResult = add(num1, num2);
   console.log(result);
+  console.log(stringResult);
 })
